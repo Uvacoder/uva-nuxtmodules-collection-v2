7e6a96e2d49f9186796109e71d13d1c446105ec8
uvacoder <uva22940coder@gmail.com>
2023-02-15 10:15:43 -0500

    Update nuxt.config.ts

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,10 +1,42 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
+import '@unocss/nuxt'
+
+const title = 'Explore Nuxt Modules'
+const description = 'Discover our list of modules to supercharge your Nuxt project. Created by the Nuxt team and community.'
+const url = 'https://modules.nuxtjs.org'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'min-h-screen'
+      },
+      title,
+      meta: [
+        { hid: 'charset', charset: 'utf-8' },
+        { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:site_name', property: 'og:site_name', content: title },
+        { hid: 'og:description', property: 'og:description', content: description },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: url },
+        { hid: 'og:image', property: 'og:image', content: `${url}/preview.png` },
+        // Twitter Card
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@nuxt_js' },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        { hid: 'twitter:description', name: 'twitter:description', content: description },
+        { hid: 'twitter:image', name: 'twitter:image', content: `${url}/preview.png` },
+        { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: title }
+      ],
+      link: [
+        { hid: 'favicon', rel: 'icon', type: 'image/png', href: '/icon.png' },
+        { hid: 'search', rel: 'search', type: 'application/opensearchdescription+xml', href: '/search.xml', title: 'Nuxt Modules' }
+      ]
+    }
+  },
   components: true,
-  buildModules: [
-    // https://github.com/windicss/nuxt-windicss
-    'nuxt-windicss',
+  modules: [
     // https://image.nuxtjs.org
     // '@nuxt/image',
     // https://pwa.nuxtjs.org
@@ -12,22 +44,20 @@ export default defineNuxtConfig({
     // https://github.com/moritzsternemann/vue-plausible
     'vue-plausible',
     // https://html-validator.nuxtjs.org
-    '@nuxtjs/html-validator',
-    '@nuxtjs/color-mode',
-    '@unocss/nuxt'
+    // '@nuxtjs/html-validator',
+    '@unocss/nuxt',
+    // https://vueuse.org
+    '@vueuse/nuxt',
+    // https://color-mode.nuxtjs.org
+    '@nuxtjs/color-mode'
   ],
-  unocss: {
-    include: [/\.vue$/, /\.ts$/, /\.vue\?vue/],
-    icons: {
-      scale: 1.2,
-      extraProperties: {
-        display: 'inline-block'
-      }
-    },
-    // disable the default preset, Windi CSS already handles them
-    uno: false
+  colorMode: {
+    classSuffix: ''
   },
-  plugins: ['~/plugins/v-tooltip.ts', '~/plugins/modules.ts'],
+  unocss: {
+    preflight: false,
+    autoImport: false
+  },
   manifest: {
     name: 'Nuxt Modules',
     short_name: 'Nuxt Modules',
@@ -37,12 +67,14 @@ export default defineNuxtConfig({
   plausible: {
     domain: 'modules.nuxtjs.org'
   },
-  colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    classSuffix: ''
-  },
-  bridge: {
-    vite: true
+  vue: {
+    compilerOptions: {
+      directiveTransforms: {
+        tooltip: () => ({
+          props: [],
+          needRuntime: true
+        })
+      }
+    }
   }
 })
